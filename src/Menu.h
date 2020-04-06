247e78e3b6d1035d1f44004ffdb28d36ec97bc7a
Daniel White <xt1zer@outlook.com>
2020-04-06 23:43:04 +0300

    Fix head and tail confusion; add list sort

--- src/Menu.h
+++ src/Menu.h
@@ -3,7 +3,7 @@
 #include <fstream>
 #include "DoublyList.h"
 
-/* A universay way to clear console screen without using system calls */
+/* A universal way to clear console screen without using system calls */
 #ifdef _WIN32
 #include <windows.h>
 void clearScreen() {
@@ -27,18 +27,18 @@ void clearScreen() {
         cellCount,
         homeCoords,
         &count
-        )) return;
+    )) return;
 
-    /* Fill the entire buffer with the current colors and attributes */
+/* Fill the entire buffer with the current colors and attributes */
     if (!FillConsoleOutputAttribute(
         hStdOut,
         csbi.wAttributes,
         cellCount,
         homeCoords,
         &count
-        )) return;
+    )) return;
 
-    /* Move the cursor home */
+/* Move the cursor home */
     SetConsoleCursorPosition(hStdOut, homeCoords);
 }
 #endif
@@ -58,7 +58,7 @@ void clearScreen() {
 }
 #endif
 
-void save(DoublyList<int>& list, const std::string& fileName) {
+void save(DoublyList<int> &list, const std::string &fileName) {
     std::ofstream outFile(fileName);
 
     while (!outFile.is_open()) {
@@ -77,7 +77,7 @@ void save(DoublyList<int>& list, const std::string& fileName) {
     std::cout << "List saved to file!\n";
 }
 
-void load(DoublyList<int>& list, const std::string fileName) {
+void load(DoublyList<int> &list, const std::string fileName) {
     std::ifstream inFile(fileName);
 
     int i = 0;
@@ -95,108 +95,118 @@ void load(DoublyList<int>& list, const std::string fileName) {
 
     int data;
     while (inFile >> data)
-        list.pushToHead(data);
+        list.pushToTail(data);
 
     inFile.close();
     std::cout << "List created from file!\n";
 }
 
-void choice(const int& c, DoublyList<int>& list) {
+void choice(const int &c, DoublyList<int> &list) {
     clearScreen();
 
     switch (c) {
-    case 0: {
-        if (list.isEmpty())
-            std::cout << "List is empty\n";
-        else {
-            std::cout << std::endl;
-            DoublyList<int>::Iterator it = list.getTail();
-            while (it) {
-                std::cout << list.getValue(it) << " ";
-                it = list.getNext(it);
+        case 0: {
+            if (list.isEmpty())
+                std::cout << "List is empty\n";
+            else {
+                DoublyList<int>::Iterator it = list.getHead();
+                while (it) {
+                    std::cout << list.getValue(it) << " ";
+                    it = list.getNext(it);
+                }
+                std::cout << std::endl;
             }
-            std::cout << std::endl;
+            break;
         }
-        break;
-    }
 
-    case 1: {
-        int value;
-        std::cout << "Enter value: ";
-        std::cin >> value;
-        list.pushToHead(value);
-        break;
-    }
+        case 1: {
+            int value;
+            std::cout << "Enter value: ";
+            std::cin >> value;
+            list.pushToTail(value);
+            break;
+        }
 
-    case 2: {
-        int value;
-        std::cout << "Enter value: ";
-        std::cin >> value;
-        list.pushToTail(value);
-        break;
-    }
+        case 2: {
+            int value;
+            std::cout << "Enter value: ";
+            std::cin >> value;
+            list.pushToHead(value);
+            break;
+        }
 
-    case 3: {
-        int value, pos;
-        std::cout << "Enter value and position: ";
-        std::cin >> value >> pos;
-        list.pushAfter(value, pos);
-        break;
-    }
+        case 3: {
+            int value, pos;
+            std::cout << "Enter value and position: ";
+            std::cin >> value >> pos;
+            list.pushAfter(value, pos);
+            break;
+        }
 
-    case 4: {
-        int position;
-        std::cout << "Select element position: ";
-        std::cin >> position;
-        std::cout << "Popped value " << list.pop(--position) << std::endl;
-        break;
-    }
+        case 4: {
+            int position;
+            std::cout << "Select element position: ";
+            std::cin >> position;
+            std::cout << "Popped value " << list.pop(--position) << std::endl;
+            break;
+        }
 
-    case 5: {
-        list.clear();
-        std::cout << "List cleared\n";
-        break;
-    }
+        case 5: {
+            if (list.isEmpty())
+                std::cout << "List is empty\n";
+            else {
+                list.sort();
+                std::cout << "List sorted!\n";
+            }
+            break;
+        }
 
-    case 6: {
-        if (list.isEmpty())
-            std::cout << "List is empty\n";
-        else
-            std::cout << "List is not empty\n";
-        break;
-    }
+        case 6: {
+            list.clear();
+            std::cout << "List cleared\n";
+            break;
+        }
 
-    case 7: {
-        std::string fileName;
-        std::cout << "Enter filename: ";
-        std::cin >> fileName;
-        save(list, fileName);
-        break;
-    }
+        case 7: {
+            if (list.isEmpty())
+                std::cout << "List is empty\n";
+            else
+                std::cout << "List is not empty\n";
+            break;
+        }
 
-    case 8: {
-        std::string fileName;
-        std::cout << "Enter filename: ";
-        std::cin >> fileName;
-        load(list, fileName);
-        break;
-    }
+        case 8: {
+            std::string fileName;
+            std::cout << "Enter filename: ";
+            std::cin >> fileName;
+            save(list, fileName);
+            break;
+        }
+
+        case 9: {
+            std::string fileName;
+            std::cout << "Enter filename: ";
+            std::cin >> fileName;
+            load(list, fileName);
+            break;
+        }
     }
 }
 
-void menu(DoublyList<int>& list) {
+void menu(DoublyList<int> &list) {
     int c;
     while (true) {
         std::cout << "Options:\n\n";
         std::cout << "0. Print list\n";
-        std::cout << "1. Push element after head\n";
-        std::cout << "2. Push element before tail\n";
+        std::cout << "1. Push element to end\n";
+        std::cout << "2. Push element to beginning\n";
         std::cout << "3. Push element after position\n";
         std::cout << "4. Pop element on position\n";
-        std::cout << "5. Clear list\n";
-        std::cout << "6. Check if empty\n";
-        std::cout << "7. Save to file\n";
-        std::cout << "8. Read from file\n";
+        std::cout << "5. Sort list\n";
+        std::cout << "6. Clear list\n";
+        std::cout << "7. Check if empty\n";
+        std::cout << "8. Save to file\n";
+        std::cout << "9. Read from file\n";
 
         std::cin >> c;
 
